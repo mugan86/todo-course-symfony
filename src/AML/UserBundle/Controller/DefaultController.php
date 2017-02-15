a336095e3e1f015871372b645e4580a20ee1470c
anartzmugika <mugan86@gmail.com>
2017-02-15 19:42:49 +0100

    Update with default index and hello name action

--- src/AML/UserBundle/Controller/DefaultController.php
+++ src/AML/UserBundle/Controller/DefaultController.php
@@ -8,6 +8,10 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AMLUserBundle:Default:index.html.twig');
+        return $this->render('AMLUserBundle:Default:index.html.twig', array('name' => 'AMLUserBundle'));
+    }
+    public function helloNameAction($name)
+    {
+        return $this->render('AMLUserBundle:Default:index.html.twig', array('name' => $name));
     }
 }
