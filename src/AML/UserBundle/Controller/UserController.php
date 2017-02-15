d48967844aadf081365bc0c5ca60751f8bbf5310
anartzmugika <mugan86@gmail.com>
2017-02-15 19:57:03 +0100

    Add new actions and routes

--- src/AML/UserBundle/Controller/UserController.php
+++ src/AML/UserBundle/Controller/UserController.php
@@ -3,15 +3,21 @@
 namespace AML\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AMLUserBundle:Default:index.html.twig', array('name' => 'AMLUserBundle/UserController Index!'));
+        return new Response('Bienvenido a mi módulo de usuarios');
     }
     public function showUserAction($id)
     {
         return $this->render('AMLUserBundle:Default:index.html.twig', array('name' => '---> Show user ' . $id . ' info!'));
+    }
+
+    public function articlesAction($page)
+    {
+      return new Response('Este es mi artículo de la página ' . $page);
     }
 }
